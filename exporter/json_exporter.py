de573e8ee84d1c7f7066ab1c1180b531bb88f33d
wsc7758 <757878213@qq.com>
2026-07-22 12:26:59 +0800

    Create json_exporter.py

---
+++ exporter/json_exporter.py
@@ -0,0 +1,7 @@
+import json
+import os
+from datetime import datetime
+from config.constants import OUTPUT_LATEST, OUTPUT_ARCHIVE, JSON_RESULT_NAME
+
+def export_json(final_result, sentiment_info):
+    today = datetime
